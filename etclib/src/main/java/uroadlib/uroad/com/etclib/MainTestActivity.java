fc3b140935fb6fe36c19ae26a7ec325dd46208d7
王喜航 <wangxihang@u-road.com>
2016-11-07 15:25:36 +0800

    构建基础库2

---
+++ etclib/src/main/java/uroadlib/uroad/com/etclib/MainTestActivity.java
@@ -0,0 +1,10 @@
+package uroadlib.uroad.com.etclib;
+
+import android.app.Activity;
+
+/**
+ * Created by 王喜航 on 2016/11/7.
+ */
+
+public class MainTestActivity extends Activity {
+}
---
+++ uroaduilib/src/main/java/uroadlib/uroad/com/uroaduilib/MainTest.java
@@ -0,0 +1,10 @@
+package uroadlib.uroad.com.uroaduilib;
+
+import android.app.Activity;
+
+/**
+ * Created by 王喜航 on 2016/11/7.
+ */
+
+public class MainTest extends Activity{
+}
