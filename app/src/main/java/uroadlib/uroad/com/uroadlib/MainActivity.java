9a6dcadf58030a88774ad8b5124e25d3c85f3621
王喜航 <wangxihang@u-road.com>
2016-11-07 10:29:22 +0800

    构建基础库

---
+++ app/src/main/java/uroadlib/uroad/com/uroadlib/MainActivity.java
@@ -0,0 +1,13 @@
+package uroadlib.uroad.com.uroadlib;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
